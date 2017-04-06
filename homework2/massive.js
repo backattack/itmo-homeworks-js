36b2b20ae93900270b6bc1a56b1229e8978f82ca
chuckattak <kovzhut@outlook.com>
2017-04-06 17:13:58 +0300

    .

--- homework2/massive.js
+++ homework2/massive.js
@@ -58,7 +58,7 @@ console.log(massive);
 
 */
 
-/*   Определить - является ли треугольник с заданными координатами прямоугольным */
+/*   Определить - является ли треугольник с заданными координатами прямоугольным
 
 var ax, ay, bx, by, cx, cy, a, b, c;
 ax = prompt('Введите X для вершины A');
@@ -67,14 +67,15 @@ bx = prompt('Введите X для вершины B');
 by = prompt('Введите Y для вершины B');
 cx = prompt('Введите X для вершины C');
 cy = prompt('Введите Y для вершины C');
-a = Math.round(Math.sqrt(Math.pow(bx - ax, 2) + Math.pow(by - ay, 2)));
+a = Math.round(Math.sqrt((bx - ax) + (by - ay)));
 console.log('Сторона A равна = ', a);
-b = Math.round(Math.sqrt(Math.pow(cx - bx, 2) + Math.pow(cy - by, 2)));
+b = Math.round(Math.sqrt((cx - bx) + (cy - by)));
 console.log('Сторона B равна = ', b);
-c = Math.round(Math.sqrt(Math.pow(cx - ax, 2) + Math.pow(cy - ay, 2)));
+c = Math.round(Math.sqrt((cx - ax) + (cy - ay)));
 console.log('Сторона C равна = ', c);
 if (a + b === c || a + c === b || b + c === a) {
     console.log('Ваш треугольник прямоугольный');
 } else {
     console.log('Не тот треугольник!');
 }
+*/
