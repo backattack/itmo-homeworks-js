8082461c815676dda00cc64d82f7e2d46d8fa0b0
chuckattak <kovzhut@outlook.com>
2017-04-17 21:40:13 +0300

    .

--- homework8/javascript/task1.js
+++ homework8/javascript/task1.js
@@ -1,6 +1,21 @@
 'use strict'
-// Задача №1
 // Даны несколько div элементов на html. По первому нажатию на
+// Задача №1
 // каждый div он перекрашивается зеленым цветом, при повторном
 // нажатии перекрашивается обратно и так далее каждый клик
 // происходит чередование цвета.
+
+var firstDiv = document.getElementById('obj1');
+
+firstDiv.onclick = function() {
+    switch (this.style.backgroundColor) {
+        case this.style.backgroundColor = "purple":
+            this.style.backgroundColor = "green"
+            this.style.color = "white"
+            break;
+        case this.style.backgroundColor = "green":
+            this.style.backgroundColor = "purple"
+            this.style.color = "black"
+            break;
+    };
+};
